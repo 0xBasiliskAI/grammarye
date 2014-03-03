85485e81495521db6d128138431204890d14df14
wizardbeard <ramonjlongiii@gmail.com>
2014-03-02 21:48:02 -0500

    Modifying project.clj to have a meaningful description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject grammarye "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small web app for invoking Apache OpenNLP in Clojure."
+  :url "http://grammarye.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.8.1"]
                  [compojure "1.1.6"]
