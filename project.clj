84778ef0e76fa25206b17da2645fb44d4fdd7cfd
wizardbeard <ramonjlongiii@gmail.com>
2014-03-02 21:37:11 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,28 @@
+(defproject grammarye "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [lib-noir "0.8.1"]
+                 [compojure "1.1.6"]
+                 [ring-server "0.3.1"]
+                 [selmer "0.6.1"]
+                 [com.taoensso/timbre "3.0.0"]
+                 [com.taoensso/tower "2.0.1"]
+                 [markdown-clj "0.9.41"]
+                 [environ "0.4.0"]]
+
+  :repl-options {:init-ns grammarye.repl}
+  :plugins [[lein-ring "0.8.10"]
+            [lein-environ "0.4.0"]]
+  :ring {:handler grammarye.handler/app
+         :init    grammarye.handler/init
+         :destroy grammarye.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production {:ring {:open-browser? false
+                       :stacktraces?  false
+                       :auto-reload?  false}}
+   :dev {:dependencies [[ring-mock "0.1.5"]
+                        [ring/ring-devel "1.2.1"]]
+         :env {:dev true}}}
+  :min-lein-version "2.0.0")
